291f2ee6ecb9d2ebe09ee8eac361e31e5a2bf1f2
christopher-besch <christopher.besch@gmx.de>
2022-01-13 11:17:36 +0100

    added regex; client-side code works

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Quote Rater
+
+## References
+
+- [useState](https://github.com/h-dong/gatsby-blog/blob/master/src/components/Header.js)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Layout from "src/components/layout";
 import * as styles from "src/styles/home.module.scss";
 import Heading from "src/components/heading";
 
+let rates_per_quote = 5;
+let quotes_url: string | undefined = undefined;
+
 const Home: React.FC = (props) => {
+    const [show_cards, set_show_cards] = useState(false);
+    const [left_card, set_left_card] = useState("left_card");
+    const [right_card, set_right_card] = useState("right_card");
+
+    const quote_regex = /### +(.*)\n```\n((?:.|\n)*?)\n—(.*)\n```\n((?:- +.+\n)*)/gm;
+
+    function onload() {
+
+    }
+
+    function update() {
+        let url_serach_params = new URLSearchParams(location.search);
+        console.log(url_serach_params.get("test"));
+        console.log(rates_per_quote);
+        rates_per_quote = 10;
+        set_show_cards(true);
+    }
+
     return (
         <Layout>
             <Heading heading="Quote Rater" />
-        </Layout>
+            <button onClick={update} onLoad={onload}>Update</button>
+            {show_cards && (
+                <div className={styles.cards}>
+                    <div className={styles.card}>
+                        <pre><code>
+                            {left_card}
+                        </code></pre>
+                    </div>
+                    <div className={styles.card}>
+                        <pre><code>
+                            {right_card}
+                        </code></pre>
+                    </div>
+                </div>
+            )
+            }
+        </Layout >
     );
 };
 export default Home;
