04c67f2486a247a04c7777159cc3c90c91c17802
christopher-besch <christopher.besch@gmx.de>
2022-01-13 23:07:28 +0100

    useState broken

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Layout from "src/components/layout";
 import * as styles from "src/styles/home.module.scss";
 import Heading from "src/components/heading";
 
-let rates_per_quote = 5;
-let quotes_url: string | undefined = undefined;
+const quote_regex = /### +(.*)\n```\n((?:.|\n)*?)\n—(.*)\n```\n((?:- +.+\n)*)/gm;
+
+type Quote = {
+    origin: string;
+    text: string;
+    author: string;
+    comments: string;
+};
 
 const Home: React.FC = (props) => {
-    const [show_cards, set_show_cards] = useState(false);
-    const [left_card, set_left_card] = useState("left_card");
-    const [right_card, set_right_card] = useState("right_card");
+    let [show_cards, set_show_cards] = useState(false);
+    let [left_card, set_left_card] = useState("left_card");
+    let [right_card, set_right_card] = useState("right_card");
+    let [quotes_url, set_quotes_url] = useState("");
 
-    const quote_regex = /### +(.*)\n```\n((?:.|\n)*?)\n—(.*)\n```\n((?:- +.+\n)*)/gm;
+    let url_search_params: URLSearchParams;
 
-    function onload() {
+    let quotes: Quote[];
+    let adj: number[][];
 
+    useEffect(() => {
+        url_search_params = new URLSearchParams(location.search);
+        let quotes_url_raw = url_search_params.get("quotes_url");
+
+        set_quotes_url(quotes_url_raw ? quotes_url_raw : "");
+    });
+
+    function dwn(url: string, callback: { (response: string): void; }): void {
+        let request = new XMLHttpRequest();
+        request.onload = () => {
+            if (request.status == 200)
+                callback(request.responseText);
+            else {
+                console.error(`Failed to load json '${url}' with status ${request.status}`);
+            }
+        };
+        request.onerror = () => {
+            console.error(`Failed to load json '${url}'`);
+        };
+        request.open("GET", url, true);
+        request.send();
     }
 
-    function update() {
-        let url_serach_params = new URLSearchParams(location.search);
-        console.log(url_serach_params.get("test"));
-        console.log(rates_per_quote);
-        rates_per_quote = 10;
-        set_show_cards(true);
+    function load() {
+        let quotes_url_input = document.getElementById("quotes_url_input") as HTMLButtonElement;
+        set_quotes_url(quotes_url_input.value);
+        url_search_params.set("quotes_url", quotes_url);
+        window.history.replaceState({}, "", `${location.pathname}?${url_search_params.toString()}`);
+        console.log(quotes_url_input.value);
+        console.log(quotes_url);
+
+        // dwn(quotes_url, (response) => {
+        //     let match;
+        //     while (match = quote_regex.exec(response)) {
+        //         quotes.push({ origin: match[1], text: match[2], author: match[3], comments: match[4] });
+        //         adj.push([]);
+        //     }
+        //     console.log(quotes);
+        //     console.log(adj);
+        // });
+    }
+
+    function left_better() {
+
+    }
+    function equal() {
+
+    }
+    function right_better() {
+
     }
 
     return (
         <Layout>
             <Heading heading="Quote Rater" />
-            <button onClick={update} onLoad={onload}>Update</button>
+            <label htmlFor="quotes_url_input">Input Quotes URL: </label>
+            <input id="quotes_url_input" type="text" placeholder={quotes_url}></input><br />
+            <button onClick={load}>Load</button>
+
             {show_cards && (
                 <div className={styles.cards}>
                     <div className={styles.card}>
@@ -42,9 +95,13 @@ const Home: React.FC = (props) => {
                             {right_card}
                         </code></pre>
                     </div>
+                    <button onClick={left_better}>Left Better</button>
+                    <button onClick={equal}>Roughly Equal</button>
+                    <button onClick={right_better}>Right Better</button>
                 </div>
             )
             }
+            <p>d{quotes_url}</p>
         </Layout >
     );
 };
