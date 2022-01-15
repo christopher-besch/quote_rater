dda696d5c14043ff1821161924da9a72f5e54c17
christopher-besch <christopher.besch@gmx.de>
2022-01-15 20:39:01 +0000

    Deploying to pages from @ christopher-besch/quote_rater@58cf0f095d6a38e2aaca855d19204edf058cb587 🚀

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-image/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
-      options: {"plugins":[],"maxWidth":8000,"linkImagesToOriginal":true,"showCaptions":false,"markdownCaptions":false,"backgroundColor":"white","quality":50,"withWebp":false,"withAvif":false,"tracedSVG":false,"loading":"lazy","decoding":"async","disableBgImageOnAlpha":false,"disableBgImage":false},
-    }]
