09604d413e37c81337e96df4fb50350c1e4b2048
christopher-besch <christopher.besch@gmx.de>
2022-01-15 11:47:24 +0100

    what a classic example of software pretending to be smarter than the user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,22 @@ import React, { useState, useEffect } from "react";
 import Layout from "src/components/layout";
 import * as styles from "src/styles/home.module.scss";
 import Heading from "src/components/heading";
-import { Quote, QuotesHandler } from "src/utils/quotes";
+import {
+    LoadedQuotes,
+    QuotesHandler,
+    get_loaded_quotes,
+    get_quotes_handler,
+    setup_loaded_quotes,
+    setup_quotes_handler,
+    next_question,
+    get_left_quote,
+    get_right_quote,
+    get_quotes_string
+} from "src/utils/quotes";
 
 const Home: React.FC = (props) => {
-    let [quotes_handler, set_quotes_handler] = useState<QuotesHandler>(new QuotesHandler());
+    let [loaded_quotes, set_loaded_quotes] = useState<LoadedQuotes>(get_loaded_quotes());
+    let [quotes_handler, set_quotes_handler] = useState<QuotesHandler>(get_quotes_handler());
 
     // run for each refresh
     useEffect(() => {
@@ -23,30 +35,29 @@ const Home: React.FC = (props) => {
         let quotes_url = quotes_url_input.value;
         url_search_params.set("quotes_url", quotes_url);
         window.history.replaceState({}, "", `${location.pathname}?${url_search_params.toString()}`);
-        let new_quotes_handler = new QuotesHandler(quotes_url, () => {
-            set_quotes_handler(new_quotes_handler);
+
+        setup_loaded_quotes(loaded_quotes, quotes_url, (new_loaded_quotes) => {
+            set_loaded_quotes(new_loaded_quotes);
+            setup_quotes_handler(quotes_handler, new_loaded_quotes);
+            set_quotes_handler((quotes_handler) => {
+                return next_question(quotes_handler);
+            });
         });
     }
 
     function left_better() {
         set_quotes_handler((quotes_handler) => {
-            quotes_handler
-
-            quotes_handler.next_question();
-            return quotes_handler;
+            return next_question(quotes_handler);
         });
     }
     function equal() {
         set_quotes_handler((quotes_handler) => {
-            quotes_handler.next_question();
-            return quotes_handler;
+            return next_question(quotes_handler);
         });
     }
     function right_better() {
         set_quotes_handler((quotes_handler) => {
-
-            quotes_handler.next_question();
-            return quotes_handler;
+            return next_question(quotes_handler);
         });
     }
 
@@ -56,21 +67,20 @@ const Home: React.FC = (props) => {
             <label htmlFor="quotes_url_input">Input Quotes URL: </label>
             <input id="quotes_url_input" type="text"></input><br />
             <button onClick={load}>Load</button>
-            <p>{quotes_handler.get_quotes_amount()}</p>
 
-            {quotes_handler.get_should_render() && (
+            {quotes_handler.should_render && (
                 <div>
-                    <p>quotes: {quotes_handler.get_quotes_amount()}</p>
-                    <p>components: {quotes_handler.get_components_amount()}</p>
+                    <p>quotes: {quotes_handler.quotes_amount}</p>
+                    <p>components: {quotes_handler.components_amount}</p>
                     <div className={styles.cards}>
                         <div className={styles.card}>
                             <pre><code>
-                                {quotes_handler.get_left_quote().text}
+                                {get_left_quote(loaded_quotes, quotes_handler).text}
                             </code></pre>
                         </div>
                         <div className={styles.card}>
                             <pre><code>
-                                {quotes_handler.get_right_quote().text}
+                                {get_right_quote(loaded_quotes, quotes_handler).text}
                             </code></pre>
                         </div>
                     </div>
@@ -79,7 +89,7 @@ const Home: React.FC = (props) => {
                     <button onClick={right_better}>Right Better</button>
                     <h1>Ordered Quotes:</h1>
                     <pre><code>
-                        {quotes_handler.get_quotes_string()}
+                        {get_quotes_string(loaded_quotes, quotes_handler)}
                     </code></pre>
                 </div>
             )}
--- src/utils/quotes.ts
+++ src/utils/quotes.ts
@@ -7,138 +7,164 @@ export type Quote = {
     comments: string;
 };
 
-export class QuotesHandler {
-    private url: string = "";
-    private should_render: boolean = true;
-    private quotes: Quote[] = [];
-    private quotes_order: number[] = [];
-
-    private adj: number[][] = [];
-    private link: number[] = [];
-    private size: number[] = [];
-    private left_idx: number = -1;
-    private right_idx: number = -1;
-    private components_amount: number = 0;
-
-    constructor(url: string = "", callback: { (): void } = () => { }) {
-        if (url == "") {
-            this.should_render = false;
-            return;
-        }
-        this.url = url;
-        this.dwn(url, (response) => {
-            let match;
-            while (match = quote_regex.exec(response)) {
-                this.quotes.push({ origin: match[1], text: match[2], author: match[3], comments: match[4] });
-            }
-            this.setup();
-            this.top_sort();
-            this.next_question();
-            callback();
-        });
-    }
-
-    public get_should_render(): boolean {
-        return this.should_render;
-    }
-
-    public next_question(): void {
-        // terribly inefficient, too bad
-        do {
-            this.left_idx = Math.floor(Math.random() * this.quotes.length);
-            this.right_idx = Math.floor(Math.random() * this.quotes.length);
-        } while (this.same(this.left_idx, this.right_idx))
-        console.log(`using quotes ${this.left_idx} and ${this.right_idx}`);
-    }
-
-    public get_quotes_string(): string {
-        let out: string = "";
-        for (let idx of this.quotes_order) {
-            let quote = this.quotes[idx];
-            out += `### ${quote.origin}\n\`\`\`\n${quote.text}\n—${quote.author}\n\`\`\`\n${quote.comments}\n`;
-        }
-        return out;
-    }
-
-    // to be executed from react
-    // because apparently this garbage requires multi-threaded-level of fuckery
-    public get_left_quote(): Quote {
-        console.log(this.left_idx);
-        return this.quotes[this.left_idx];
-    }
-    public get_right_quote(): Quote {
-        return this.quotes[this.right_idx];
-    }
-
-    public get_quotes_amount(): number {
-        return this.quotes.length;
-    }
-
-    public get_components_amount(): number {
-        return this.components_amount;
-    }
-
-    private dwn(url: string, callback: { (response: string): void }): void {
-        let request = new XMLHttpRequest();
-        request.onload = () => {
-            if (request.status == 200)
-                callback(request.responseText);
-            else {
-                console.error(`Failed to load '${url}' with status ${request.status}`);
-            }
-        };
-        request.onerror = () => {
-            console.error(`Failed to load '${url}'`);
-        };
-        request.open("GET", url, true);
-        request.send();
-    }
-
-    private setup(): void {
-        for (let i = 0; i < this.quotes.length; ++i) {
-            this.adj.push([]);
-            this.link[i] = i;
-            this.size[i] = 1;
-        }
-        this.components_amount = this.quotes.length;
-    }
-    private find(x: number): number {
-        while (x != this.link[x])
-            x = this.link[x];
-        return x;
-    }
-    private same(a: number, b: number): boolean {
-        return this.find(a) == this.find(b);
-    }
-    // don't use with nodes from same component
-    private unite(a: number, b: number): void {
-        a = this.find(a);
-        b = this.find(b);
-        if (this.size[a] < this.size[b])
-            [a, b] = [b, a];
-        this.size[a] += this.size[b];
-        this.link[b] = a;
-        --this.components_amount;
-    }
-
-    private top_sort(): void {
-        this.quotes_order = [];
-        let status: number[] = [];
-        for (let i = 0; i < this.quotes.length; ++i)
-            status.push(0);
-        for (let i = 0; i < this.quotes.length; ++i)
-            this.top(i, status);
-    }
-    private top(cur: number, status: number[]) {
-        if (status[cur] == 2)
-            return;
-        status[cur] = 1;
-
-        for (let next of this.adj[cur])
-            this.top(next, status);
-
-        this.quotes_order.push(cur);
-        status[cur] = 2;
-    }
-
+// React doesn't support mutable objects...JavaScript doesn't allow constructor overloading...RAAI through the drain...this hurts
+export type LoadedQuotes = {
+    quotes: Quote[];
+    url: string;
 };
+export function get_loaded_quotes(): LoadedQuotes {
+    return {
+        quotes: [],
+        url: "",
+    };
+}
+function copy_loaded_quotes(loaded_quotes: LoadedQuotes): LoadedQuotes {
+    return {
+        quotes: loaded_quotes.quotes,
+        url: loaded_quotes.url,
+    }
+}
+
+export type QuotesHandler = {
+    should_render: boolean;
+    quotes_order: number[];
+    adj: number[][];
+    link: number[];
+    size: number[];
+    left_idx: number;
+    right_idx: number;
+    quotes_amount: number,
+    components_amount: number;
+};
+export function get_quotes_handler(): QuotesHandler {
+    return {
+        should_render: false,
+        quotes_order: [],
+        adj: [],
+        link: [],
+        size: [],
+        left_idx: -1,
+        right_idx: -1,
+        quotes_amount: 0,
+        components_amount: 0,
+    };
+}
+function copy_quotes_handler(quotes_handler: QuotesHandler): QuotesHandler {
+    return {
+        should_render: quotes_handler.should_render,
+        quotes_order: quotes_handler.quotes_order,
+        adj: quotes_handler.adj,
+        link: quotes_handler.link,
+        size: quotes_handler.size,
+        left_idx: quotes_handler.left_idx,
+        right_idx: quotes_handler.right_idx,
+        quotes_amount: quotes_handler.quotes_amount,
+        components_amount: quotes_handler.components_amount,
+    };
+}
+
+export function setup_loaded_quotes(loaded_quotes: LoadedQuotes, url: string = "", callback: { (loaded_quotes: LoadedQuotes): void } = () => { }): void {
+    loaded_quotes.url = url;
+    dwn(url, (response) => {
+        let match;
+        while (match = quote_regex.exec(response)) {
+            loaded_quotes.quotes.push({ origin: match[1], text: match[2], author: match[3], comments: match[4] });
+        }
+        callback(copy_loaded_quotes(loaded_quotes));
+    });
+}
+
+export function setup_quotes_handler(quotes_handler: QuotesHandler, loaded_quotes: LoadedQuotes): QuotesHandler {
+    quotes_handler.quotes_amount = loaded_quotes.quotes.length;
+    quotes_handler.should_render = true;
+    for (let i = 0; i < quotes_handler.quotes_amount; ++i) {
+        quotes_handler.adj.push([]);
+        quotes_handler.link[i] = i;
+        quotes_handler.size[i] = 1;
+    }
+    quotes_handler.components_amount = quotes_handler.quotes_amount;
+    top_sort(quotes_handler);
+    return copy_quotes_handler(quotes_handler);
+}
+
+export function next_question(quotes_handler: QuotesHandler): QuotesHandler {
+    // terribly inefficient, too bad
+    do {
+        quotes_handler.left_idx = Math.floor(Math.random() * quotes_handler.quotes_amount);
+        quotes_handler.right_idx = Math.floor(Math.random() * quotes_handler.quotes_amount);
+    } while (same(quotes_handler, quotes_handler.left_idx, quotes_handler.right_idx))
+    console.log(`using quotes ${quotes_handler.left_idx} and ${quotes_handler.right_idx}`);
+    return copy_quotes_handler(quotes_handler);
+}
+
+export function get_quotes_string(loaded_quotes: LoadedQuotes, quotes_handler: QuotesHandler): string {
+    let out: string = "";
+    for (let idx of quotes_handler.quotes_order) {
+        let quote = loaded_quotes.quotes[idx];
+        out += `### ${quote.origin}\n\`\`\`\n${quote.text}\n—${quote.author}\n\`\`\`\n${quote.comments}\n`;
+    }
+    return out;
+}
+
+export function get_left_quote(loaded_quotes: LoadedQuotes, quotes_handler: QuotesHandler): Quote {
+    return loaded_quotes.quotes[quotes_handler.left_idx];
+}
+export function get_right_quote(loaded_quotes: LoadedQuotes, quotes_handler: QuotesHandler): Quote {
+    return loaded_quotes.quotes[quotes_handler.right_idx];
+}
+
+function dwn(url: string, callback: { (response: string): void }): void {
+    let request = new XMLHttpRequest();
+    request.onload = () => {
+        if (request.status == 200)
+            callback(request.responseText);
+        else {
+            console.error(`Failed to load '${url}' with status ${request.status}`);
+        }
+    };
+    request.onerror = () => {
+        console.error(`Failed to load '${url}'`);
+    };
+    request.open("GET", url, true);
+    request.send();
+}
+
+function find(quotes_handler: QuotesHandler, x: number): number {
+    while (x != quotes_handler.link[x])
+        x = quotes_handler.link[x];
+    return x;
+}
+function same(quotes_handler: QuotesHandler, a: number, b: number): boolean {
+    return find(quotes_handler, a) == find(quotes_handler, b);
+}
+// don't use with nodes from same component
+function unite(quotes_handler: QuotesHandler, a: number, b: number): void {
+    a = find(quotes_handler, a);
+    b = find(quotes_handler, b);
+    if (quotes_handler.size[a] < quotes_handler.size[b])
+        [a, b] = [b, a];
+    quotes_handler.size[a] += quotes_handler.size[b];
+    quotes_handler.link[b] = a;
+    --quotes_handler.components_amount;
+}
+
+function top_sort(quotes_handler: QuotesHandler,): void {
+    quotes_handler.quotes_order = [];
+    let status: number[] = [];
+    for (let i = 0; i < quotes_handler.quotes_amount; ++i)
+        status.push(0);
+    for (let i = 0; i < quotes_handler.quotes_amount; ++i)
+        top(quotes_handler, i, status);
+}
+function top(quotes_handler: QuotesHandler, cur: number, status: number[]) {
+    if (status[cur] == 2)
+        return;
+    status[cur] = 1;
+
+    for (let next of quotes_handler.adj[cur])
+        top(quotes_handler, next, status);
+
+    quotes_handler.quotes_order.push(cur);
+    status[cur] = 2;
+}
+
